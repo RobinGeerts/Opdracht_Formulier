b9d429884b64711700e099fa0a54df81b553a568
RobinGeerts <papichulio1@hotmail.com>
2023-03-11 10:36:21 +0100

    poging tot controle empty field

--- Formulier.js
+++ Formulier.js
@@ -1,3 +1,16 @@
+//declaratie 
+//const voornaam = document.getElementById('voornaam');
+//const naam = document.getElementById('naam');
+//const gebruiker = document.getElementById('gebruiker');
+//const email = document.getElementById('e-mail');
+//const wachtwoord = document.getElementById('wachtwoord');
+//const herhaalwachtwoord = document.getElementById('herhaalWachtwoord');
+//const adres = document.getElementById('adres');
+//const land = document.getElementById('land');
+//const provincie = document.getElementById('provincie');
+//const postcode = document.getElementById('postcode');
+let errors = ["<h4>Yikes, errors..</h4>"];
+
 //Bij het klikken op de knop trigger je de functie validateForm. 
 //In deze functie controleer je alle velden of spreek je eventueel andere functies aan.
 //Maak gebruik van een array errors waarin je iedere melding gaat bewaren.
@@ -10,15 +23,29 @@
 // PROVINCIE
 //Indien het veld leeg is geef je bijvoorbeeld volgende melding “Het veld voornaam is vereist.”.
 function validateForm(){
-
-document.getElementById('controle').innerHTML = "<div class=\"alert alert-danger\" role=\"alert\">This is a danger alert—check it out!";
+    checkEmptyField("voornaam", "<p>Het veld voornaam is vereist.</p>");
+    checkEmptyField("naam", "<p>Het veld naam is vereist.</p>");
+    checkEmptyField("gebruiker", "<p>Het veld gebruikersnaam is vereist.</p>");
+    checkEmptyField("e-mail", "<p>Het veld e-mail is vereist.</p>");
+    checkEmptyField("wachtwoord","<p>Het veld wachtwoord is vereist.</p>");
+    checkEmptyField("herhaalWachtwoord", "<p>Het veld herhaalwachtwoord is vereist.</p>");
+    checkEmptyField("adres", "<p>Het veld adres is vereist.</p>");
+    checkEmptyField("land", "<p>Het veld land is vereist.</p>");
+    checkEmptyField("provincie", "<p>Het veld provincie is vereist.</p>");
+    checkEmptyField("postcode", "<p>Het veld postcode is vereist.</p>");
+document.getElementById('controle').innerHTML = "<div class=\"alert alert-danger\" role=\"alert\">" + errors;
 }
 
 //Voor de controle van een leeg veld gebruik je een functie checkEmptyField waar je steeds 2 parameters 
 //gaat meegeven: veld en melding. Voeg na controle in deze functie de melding toe aan de array errors. 
 function checkEmptyField(veld, melding){
-
+    if(document.getElementById(veld).value == "")
+    {
+        errors.push(melding);
+    }
 }
+
+
 
 //Voor de controle of het een geldig e-mailadres is gebruik je de functie validateEmail waar je één 
 //parameter meegeeft namelijk: e-mailadres. Deze functie geeft een boolean terug. 
@@ -73,11 +100,26 @@ function validatePayment(veld){
 //Geef anders de melding “De waarde van postcode moet tussen 1000 en 9999 liggen.”.
 //Voeg deze meldingen toe aan de array errors. 
 function checkPC(){
-
+    if (postcode >=1000 && postcode < 10000)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 //Controleer ook of de algemene voorwaarden zijn aangeduid. Dit is verplicht. 
 //Voeg een melding toe aan de array errors.
 function checkUseAgreement(id){
-
+const validate = getElementById('checkbox_1');
+if(validate = true)
+{
+    return true;
+}
+else 
+{
+    return false;
+}
 }
